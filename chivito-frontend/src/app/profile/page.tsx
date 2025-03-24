f414d3c77beace181f85cccc5c382e095da2296d
luiscolon0426 <lusantiago@protonmail.com>
2025-03-24 13:29:03 -0400

    update profile page

--- chivito-frontend/src/app/profile/page.tsx
+++ chivito-frontend/src/app/profile/page.tsx
@@ -1,27 +1,28 @@
 "use client";
 
 import { useState } from "react";
-import ProfileScreen from "../components/profile";
 import LoginScreen from "../auth/login/login";
-import router from "next/router";
+import ProfileScreen from "../components/profile";
+
+interface User {
+  id: string;
+  name: string;
+}
 
 export default function ProfilePage() {
-  // Simulating user authentication state
-  const [user, setUser] = useState(null); // Change `null` to `mockUser` to test
+  const [user, setUser] = useState<User | null>(null);
 
-  // TODO:if user is not logged in, show the login screen, if user is logged in, show the profile screen
   return (
     <div className="min-h-screen bg-gray-100">
       {user ? (
-        <ProfileScreen />
+        <ProfileScreen user={user} />
       ) : (
-        <>
-          <LoginScreen onLogin={(userData) => setUser(userData)} />
-          <button onClick={() => router.push("/profile")}>Go to Profile</button>
-        </>
+        <LoginScreen
+          onLogin={(userData: User) => {
+            setUser(userData);
+          }}
+        />
       )}
     </div>
   );
 }
-/* <ProfileScreen /> */
-/* <LoginScreen onLogin={undefined} /> */
